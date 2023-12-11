86d69e03a0275c31f87222ff8c5e34d352625800
Sanket09876 <136728140+Sanket09876@users.noreply.github.com>
2023-12-11 12:24:05 +0530

    Add files via upload

---
+++ StudentTester.java
@@ -0,0 +1,10 @@
+public class StudentTester{
+   
+    public static void main(String[] args){
+        Student student = new Student();
+        student.setStudentDetails("SANKET", 1007, 98.0, 3, "Hostellers");
+        student.displayStudentDetails();
+
+ }
+}
+ 
